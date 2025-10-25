109791824d25cbd6b827b05610d3d87a6274de6f
damikb10 <damikb10@gmail.com>
2025-10-25 17:31:07 -0400

    Update import path for toNodeHandler

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,4 +1,4 @@
 import { auth } from "../../../../lib/auth";
-import { toNodeHandler } from "better-auth/nextjs";
+import { toNodeHandler } from "better-auth/node";
 
 export const { GET, POST } = toNodeHandler(auth);
